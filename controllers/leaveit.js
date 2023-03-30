4268b265399348181a035d909dff99da562a6031
EGORDYU <egordyuzhev@gmail.com>
2023-03-30 15:26:36 -0600

    I have no idea why its different from the gitbook

--- controllers/leaveit.js
+++ controllers/leaveit.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/movies', (req, res) => {
-    res.render('/leaveit/movies', {movies: ['Siegfried', 'Any movie he recently showed']})
+    res.render('movies', {movies: ['Siegfried', 'Any movie he recently showed']})
   });
 
   router.get('/products', (req, res) => {
-    res.render('/leaveit/products', {products: ['Cocaine', 'Blood Diamonds']})
+    res.render('products', {products: ['Cocaine', 'Blood Diamonds']})
   });
 
 
